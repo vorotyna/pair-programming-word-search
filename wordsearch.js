a51136658caeceaa7f6523631efee4c3d31f044a
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-10-26 23:20:46 +0000

    Partner challenge

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require('/vagrant/async/matrix_transposition');
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (const v of verticalJoin) {
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch; 
